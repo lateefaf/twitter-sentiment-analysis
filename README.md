75dead1417ec0e156586348b8c9c5f142599a6a7
Afnan <alateef4@gmail.com>
2023-06-18 22:55:05 -0400

    Updates kafka-python and utilizes config file as store of sensitive info.

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # twitter-sentiment-analysis
-Data engineering project that involves processing and analyzing a large dataset from Twitter to perform sentiment analysis. We'll use Apache Flink, Apache Kafka, Apache Hadoop, Python, and SQL for this example.
+Data engineering project that involves processing and analyzing a large dataset from Twitter to perform sentiment analysis. Will use Apache Flink, Apache Kafka, Apache Hadoop, Python, and SQL for this.
--- kafka_producer.py
+++ kafka_producer.py
@@ -1,27 +1,29 @@
-from kafka import KafkaProducer
+from kafka import KafkaProducer, producer
 import tweepy
+import config
+# Configure the Kafka producer
+bootstrap_servers = config.bootstrap_servers
+kafkaproducer = KafkaProducer(bootstrap_servers=bootstrap_servers)
 
-#Configure the Kafka producer
-bootstrap_servers = 'localhost:9092'
-producer = KafkaProducer(bootstrap_servers=bootstrap_servers)
+# Set the API keys and access tokens as environment variables or directly pass them to the producer.
+api_key = config.api_key
+api_secret_key = config.api_secret_key
+access_token = config.access_token
+access_token_secret = config.access_token_secret
 
-#Set the API keys and access tokens as environment variables or directly pass them to the producer.
-api_key = 'YOUR_API_KEY'
-api_secret_key = 'YOUR_API_SECRET_KEY'
-access_token = 'YOUR_ACCESS_TOKEN'
-access_token_secret = 'YOUR_ACCESS_TOKEN_SECRET'
-
-#Set up a connection to the Twitter API using your API keys and access tokens.
+# Set up a connection to the Twitter API using your API keys and access tokens.
 auth = tweepy.OAuthHandler(api_key, api_secret_key)
 auth.set_access_token(access_token, access_token_secret)
+
+# Twitter API object
 api = tweepy.API(auth)
 
-#Use the Twitter API to fetch tweets based on specific search criteria
-search_query = 'your_search_query'
-tweets = api.search(q=search_query, count=10)  # Fetch 10 tweets
+# Use the Twitter API to fetch tweets based on specific search criteria
+search_query = 'us politics'
+tweets = api.search_tweets(q=search_query, count=10)  # Fetch 10 tweets
 
-#Publish the fetched tweets to Kafka topics
-topic = 'your_topic'
+# Publish the fetched tweets to Kafka topics
+topic = 'politics'
 for tweet in tweets:
     tweet_data = tweet.text.encode('utf-8')
     producer.send(topic, value=tweet_data)
